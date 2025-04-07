05255c2ea53600fefc700e411fbdcdf23cde12b6
Jamie <jbarbourmoore@gmail.com>
2025-04-07 12:58:37 -0400

    Built more information into plantuml for caesarcypher

--- GeneratingDiagrams/BasicSequenceDiagramSetup.py
+++ GeneratingDiagrams/BasicSequenceDiagramSetup.py
@@ -18,9 +18,9 @@ class BasicSequenceDiagramSetup():
 
         self.title = title
         self.participants = {}
-        self.communications = {}
+        self.events = {}
         self.number_of_participants = 0
-        self.number_of_communications = 0
+        self.number_of_events = 0
 
         if participants_list == None:
             if messages_list != None:
@@ -32,10 +32,10 @@ class BasicSequenceDiagramSetup():
             if messages_list != None:
                 communications_length = len(messages_list)
                 for i in range(0, communications_length):
-                    self.addCommunicationFromTuple(communication_tuple=messages_list[i], index=i)
+                    self.addEventsFromTuple(event_tuple=messages_list[i], index=i)
             else:
-                self.number_of_communications = 0
-                self.communications = {}
+                self.number_of_events = 0
+                self.events = {}
 
     def addParticipant(self, participant_name, index = None):
         '''
@@ -48,23 +48,27 @@ class BasicSequenceDiagramSetup():
                 The index for the participant, default is None (self.number_of_participants)
         '''
 
-        self.participants[self.number_of_participants if index == None else index] = BasicParticipant(participant_name)
+        self.participants[self.number_of_participants if index == None else index] = BasicParticipant(participant_name, self.number_of_participants if index == None else index)
         self.number_of_participants += 1
 
-    def addCommunicationFromTuple(self, communication_tuple, index = None):
+    def addEventsFromTuple(self, event_tuple, index = None):
         '''
         This method adds a message to the sequence diagram from a tuple
 
         Parameters :
-            communication_tuple : (int,int,str)
+            communication_tuple : (str,int,int,str)
                 he message as a tuple with the start participant, end participant, and message string
             index : int, optional
                 The index for the participant, default is None (self.number_of_communications)
         '''
 
-        start_participant_number, end_participant_number, message = communication_tuple
-        self.communications[self.number_of_communications if index == None else index] = BasicCommunication(message=message, start_participant=self.participants[start_participant_number], end_participant=self.participants[end_participant_number])
-        self.number_of_communications += 1
+        if event_tuple[0] == "Message":
+            _,start_participant_number, end_participant_number, message = event_tuple
+            self.events[self.number_of_events if index == None else index] = BasicCommunication(message=message, start_participant=self.participants[start_participant_number], end_participant=self.participants[end_participant_number])
+        if event_tuple[0] == "Note":
+            _,note_content, position, participant_number = event_tuple
+            self.events[self.number_of_events if index == None else index] = BasicNote(note_content=note_content,position=position,participant=(self.participants[participant_number] if participant_number!=None else None))
+        self.number_of_events += 1
 
     def addCommunication(self, message, start_participant_number, end_participant_number):
         '''
@@ -79,8 +83,8 @@ class BasicSequenceDiagramSetup():
                 The content of the message
         '''
 
-        self.communications = BasicCommunication(message=message, start_participant=self.participants[start_participant_number], end_participant=self.participants[end_participant_number])
-        self.number_of_communications += 1
+        self.events[self.number_of_events] = BasicCommunication(message=message, start_participant=self.participants[start_participant_number], end_participant=self.participants[end_participant_number])
+        self.number_of_events += 1
 
     def printMermaidSequenceDiagram(self):
         '''
@@ -91,8 +95,9 @@ class BasicSequenceDiagramSetup():
         print(f"\tTitle {self.title}")
         for i in range(0, self.number_of_participants):
             print(f"\tParticipant {self.participants[i].name}")
-        for i in range(0,self.number_of_communications):
-            print(f"\t{self.communications[i].start_participant.name} ->> {self.communications[i].end_participant.name}: {self.communications[i].message}")
+        for i in range(0,self.number_of_events):
+            if self.events[i].type == "Message":
+                print(f"\t{self.events[i].start_participant.name} ->> {self.events[i].end_participant.name}: {self.events[i].message}")
 
     def printZenUMLDiagram(self):
         '''
@@ -102,8 +107,9 @@ class BasicSequenceDiagramSetup():
         print(f"title {self.title}")
         for i in range(0, self.number_of_participants):
             print(f"@Actor \"{self.participants[i].name}\"")
-        for i in range(0,self.number_of_communications):
-            print(f"\"{self.communications[i].start_participant.name}\"->\"{self.communications[i].end_participant.name}\": {self.communications[i].message}")
+        for i in range(0,self.number_of_events):
+            if self.events[i].type == "Message":
+                print(f"\"{self.events[i].start_participant.name}\"->\"{self.events[i].end_participant.name}\": {self.events[i].message}")
 
     def printPlantUMLDiagram(self):
         '''
@@ -111,14 +117,35 @@ class BasicSequenceDiagramSetup():
         '''
 
         print("@startuml")
+        print("!theme reddress-lightblue")
+        print("hide footbox")
         print(f"title \"{self.title}\"")
+        print("autonumber")
+        print("skinparam maxMessageSize 300")
+        print("skinparam NoteBackgroundColor LightSteelBlue")
+        print("skinparam NoteBorderColor White")
+        print("skinparam ParticipantBackgroundColor Navy")
+        print("skinparam ParticipantFontColor White")
+        print("skinparam ParticipantFontSize 13")
+        print("skinparam TitleFontColor Navy")
+        print("skinparam SequenceLifeLineBorderColor Navy")
         for i in range(0, self.number_of_participants):
             print(f"Participant \"{self.participants[i].name}\"")
-        for i in range(0,self.number_of_communications):
-            print(f"\"{self.communications[i].start_participant.name}\"->\"{self.communications[i].end_participant.name}\": {self.communications[i].message}")
+        for i in range(0,self.number_of_events):
+            if self.events[i].type == "Message":
+                if self.events[i].start_participant.id == 0 and self.events[i].end_participant.id == 0:
+                    print(f"\"{self.events[i].start_participant.name}\"<-\"{self.events[i].end_participant.name}\": {self.events[i].message}")
+                else:
+                    print(f"\"{self.events[i].start_participant.name}\"->\"{self.events[i].end_participant.name}\": {self.events[i].message}")
+            if self.events[i].type == "Note":
+                print(f"hnote {self.events[i].position} {self.events[i].participant.name if self.events[i].participant != None else ""}: {self.events[i].note_content}")
         print("@enduml")
 
     def printAllDiagrams(self):
+        '''
+        This method prints all of the uml diagram types for this sequence diagram
+        '''
+
         print("- - - - - - - - - - - -")
         self.printMermaidSequenceDiagram()
         print("- - - - - - - - - - - -")
@@ -127,8 +154,18 @@ class BasicSequenceDiagramSetup():
         self.printPlantUMLDiagram()
         print("- - - - - - - - - - - -")
 
+class BasicEvent():
+    def __init__(self, type = "Event"):
+        self.type = type
 
-class BasicCommunication():
+class BasicNote(BasicEvent):
+    def __init__(self, note_content, position, participant = None):
+        self.type = "Note"
+        self.note_content = note_content
+        self.position = position
+        self.participant = participant
+
+class BasicCommunication(BasicEvent):
     '''
     This class holds the information for a basic message in a sequence diagram
     '''
@@ -146,6 +183,7 @@ class BasicCommunication():
                 The message destination
         '''
 
+        self.type = "Message"
         self.message = message
         self.start_participant = start_participant
         self.end_participant = end_participant
@@ -155,7 +193,7 @@ class BasicParticipant():
     This class holds the information for a basic participant in a sequence diagram
     '''
 
-    def __init__(self, name):
+    def __init__(self, name, id = 0):
         '''
         This method initializes the participant with a given name
 
@@ -164,9 +202,10 @@ class BasicParticipant():
                 The name of the participant
         '''
         self.name = name
+        self.id = id
 
 if __name__ == '__main__':
     participant_list = ["Originator","Bad Actor","Recipient"]
-    communication_list = [(0,2,"Hello"),(2,0,"Hi"),(1,1,"Thinking")]
+    communication_list = [("Message",0,2,"Hello"),("Message",2,0,"Hi"),("Message",1,1,"Thinking")]
     sequence_diagram = BasicSequenceDiagramSetup("Basic Sequence",participants_list=participant_list,messages_list=communication_list)
     sequence_diagram.printAllDiagrams()
--- GeneratingDiagrams/CaesarCypherSequenceDiagram.py
+++ GeneratingDiagrams/CaesarCypherSequenceDiagram.py
@@ -2,20 +2,22 @@ from CryptographySchemes.CaesarCypher import CaesarCipher
 from BasicSequenceDiagramSetup import BasicSequenceDiagramSetup
 
 if __name__ == '__main__':
-    message = "This message is being encrypted using caesar's cypher with a shift of five"
+    message = "Hello. How are you?"
     shift_value = 5
     caesar_cyper = CaesarCipher(shift_value=shift_value)
     encrypted_message = caesar_cyper.encrypt(message=message)
     decrypted_message = caesar_cyper.decrypt(encrypted_message=encrypted_message)
-    print("Caesar Cypher is a very simple example of encryption")
-    print("Each letter is simply replaced by those a certain number of space over in the alphabet")
-    print("For example 'abc' with a shift of 3 would be 'def'")
-    print("- - - - - - - - - - - -")
-    print(f"Original message: {message}")
-    print(f"Encrypted message: {encrypted_message}")
-    print(f"Decrypted message: {decrypted_message}")
+    reply = "Hi! I am doing well, thanks."
+    encrypted_reply = caesar_cyper.encrypt(message=reply)
+    decrypted_reply = caesar_cyper.decrypt(encrypted_message=encrypted_reply)
 
     participants = ["Originator","Receiver"]
-    messages = [(0,0,message),(0,1,encrypted_message),(1,1,decrypted_message)]
+    messages = [("Note","Both participants are aware of the caesar shift value, 5","across",None),
+                ("Note","Encrypting Message","left of",0),("Message",0,0,message),
+                ("Note","Transmitting Message","right of",0),("Message",0,1,encrypted_message),
+                ("Note","Decrypting Message","right of",1),("Message",1,1,decrypted_message),
+                ("Note","Encrypting Reply","right of",1),("Message",1,1,reply),
+                ("Note","Transmitting Reply","left of",1),("Message",1,0,encrypted_reply),
+                ("Note","Decrypting Reply","left of",0),("Message",0,0,decrypted_reply)]
     caesar_sequence = BasicSequenceDiagramSetup("Basic Caesar Cypher Example",participants_list=participants,messages_list=messages)
     caesar_sequence.printAllDiagrams()
