d980da55cf7af03572a04e2e1ca49af12e8c6e6a
Jamie <jbarbourmoore@gmail.com>
2025-04-08 12:03:11 -0400

    Updated multiplicative cypher diagram

--- GeneratingDiagrams/CryptographySchemeDiagramGenerators/MultiplicativeCypherSequenceDiagram.py
+++ GeneratingDiagrams/CryptographySchemeDiagramGenerators/MultiplicativeCypherSequenceDiagram.py
@@ -1,27 +1,35 @@
 from CryptographySchemes.MultiplicativeCypher import MultiplicativeCypher
 from GeneratingDiagrams.BasicSequenceDiagramSetup import BasicSequenceDiagramSetup
 
-if __name__ == '__main__':
-    message = "Hello. How are you?"
-    multiplication_value = 4
+def runBasicMultiplicativeCypherScenario():
+    '''
+    This function runs a basic multiplicative cypher scenario
+    '''
+
+    message = "The mission is scheduled for tomorrow morning"
+    multiplication_value = 5
     multiplicative_cyper = MultiplicativeCypher(multiplication_value=multiplication_value)
     encrypted_message = multiplicative_cyper.encrypt(message=message)
     decrypted_message = multiplicative_cyper.decrypt(encrypted_message=encrypted_message)
-    reply = "Hi! I am doing well, thanks."
+    reply = "Ok we will meet you at the airfield"
     encrypted_reply = multiplicative_cyper.encrypt(message=reply)
     decrypted_reply = multiplicative_cyper.decrypt(encrypted_message=encrypted_reply)
+    return message,encrypted_message,decrypted_message,reply,encrypted_reply,decrypted_reply
 
-    participants = ["Originator","Receiver"]
-    messages = [("Note","Both participants are aware of the multiplication value, 4","across",None),
-                ("Divider","Sending Message"), ("Lifeline",0,"Activate"),
-                ("Note","Encrypting Message","left of",0),("Message",0,0,message),
-                ("Note","Transmitting Message","right of",0),("Message",0,1,encrypted_message),
-                ("Lifeline",0,"Deactivate"), ("Lifeline",1,"Activate"),
-                ("Note","Decrypting Message","right of",1),("Message",1,1,decrypted_message),
-                ("Divider","Replying"),
-                ("Note","Encrypting Reply","right of",1),("Message",1,1,reply),
-                ("Note","Transmitting Reply","left of",1),("Message",1,0,encrypted_reply),
-                ("Lifeline",1,"Deactivate"), ("Lifeline",0,"Activate"),
-                ("Note","Decrypting Reply","left of",0),("Message",0,0,decrypted_reply)]
-    caesar_sequence = BasicSequenceDiagramSetup("Basic Multiplicative Cypher Example",participants_list=participants,messages_list=messages)
-    caesar_sequence.printAllDiagrams()
+def setupMultiplicativeCypherSequence(message, encrypted_message, decrypted_message, reply, encrypted_reply, decrypted_reply, multiplicative_sequence:BasicSequenceDiagramSetup):
+    '''
+    This function constructs the sequence for the basic multiplicative cypher scenario
+    '''
+    
+    multiplicative_sequence.initializeParticipants(2)
+    multiplicative_sequence.addBannerNote("Both participants are aware of the multiplication value which is 5")
+    multiplicative_sequence.addDivider("Sending A Message")
+    multiplicative_sequence.encryptSendAndDecryptMessage(0,1,message=message,encrypted_message=encrypted_message,decrypted_message=decrypted_message,deactivate_end=False)
+    multiplicative_sequence.addDivider("Sending A Reply")
+    multiplicative_sequence.encryptSendAndDecryptMessage(1,0,message=reply,encrypted_message=encrypted_reply,decrypted_message=decrypted_reply,message_label="Reply",activate_start=False)
+
+if __name__ == '__main__':
+    message, encrypted_message, decrypted_message, reply, encrypted_reply, decrypted_reply = runBasicMultiplicativeCypherScenario()
+    multiplicative_sequence = BasicSequenceDiagramSetup("Basic Multiplicative Cypher Example")
+    setupMultiplicativeCypherSequence(message, encrypted_message, decrypted_message, reply, encrypted_reply, decrypted_reply, multiplicative_sequence)
+    multiplicative_sequence.printAllDiagrams()
