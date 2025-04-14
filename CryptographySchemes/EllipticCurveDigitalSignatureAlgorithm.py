78ae9fd186e374bc0242b524e20132bf68db5d5f
Jamie <jbarbourmoore@gmail.com>
2025-04-14 16:31:09 -0400

    Updated to better reflect nist fips 168-5

--- CryptographySchemes/EllipticCurveDigitalSignatureAlgorithm.py
+++ CryptographySchemes/EllipticCurveDigitalSignatureAlgorithm.py
@@ -1,15 +1,15 @@
 import secrets
-from random import randint
 from HelperFunctions import EllipticCurveDetails
 from HelperFunctions.EllipticCurveCalculations import EllipticCurveCalculations
 from CryptographySchemes.SecureHashAlgorithm3 import SHA3_512
+from HelperFunctions.PrimeNumbers import calculateModuloInverse
 
 class EllipticCurveDigitalSignatureAlgorithm():
     '''
     This class stores the public information for the elliptic curve digital signature algorithm
     '''
 
-    def __init__(self, get_curve_functions , is_debug=False):
+    def __init__(self, get_curve_functions:list[EllipticCurveDetails.EllipticCurveWeierstrassFormDetails], is_debug=False):
         '''
         This method initializes the elliptic curve digital signature with a randomly selected curve and generator point
 
@@ -25,11 +25,14 @@ class EllipticCurveDigitalSignatureAlgorithm():
         if number_of_functions == 1:
             get_curve_function = get_curve_functions[0]
         else:
-            random_index = randint(0,number_of_functions - 1)
+            random_index = secrets.randbelow(number_of_functions)
             get_curve_function = get_curve_functions[random_index]
-
+        
+        self.sha3 = SHA3_512()
         self.curve_details = get_curve_function()
         self.curve = EllipticCurveCalculations(self.curve_details.a,self.curve_details.b,finite_field=self.curve_details.prime_modulus)
+        self.n =self.curve_details.order
+        self.length_n = len(self.intToBitString(self.n))
         self.is_debug = is_debug
         if is_debug:
             print("A elliptic curve digital signature algorithm has been initiated")
@@ -48,7 +51,7 @@ class EllipticCurveDigitalSignatureAlgorithm():
         This method calculates the public key based on the selected elliptic curve and the private key
         '''
 
-        self.public_key = self.curve.calculatedPointMultiplicationByConstant_doubleAndAddMethod(self.curve_details.generator_point,self.private_key)
+        self.public_key = self.multiplesOfG(self.private_key)
 
     def generatePrivateKey(self):
         '''
@@ -57,107 +60,372 @@ class EllipticCurveDigitalSignatureAlgorithm():
 
         self.private_key = secrets.randbelow(self.curve_details.prime_modulus)
 
-    def calculateHashOfItem(self, item_to_hash):
+    def calculateHashOfItem(self, item_to_hash:str) -> str:
         '''
-        This method calculate the sha3-512 hash digest of the message and returns it as an int
+        This method calculate the sha3-512 hash digest of the message and returns it as a bit string
 
         Parameter :
             item_to_hash : str
                 The item that is being hashed
 
         Returns :
-            hash_value : int
-                The hash value of the item as an int
+            hash_value : str
+                The hash value of the item as a bit string
         '''
 
-        sha3_512 = SHA3_512()
-        return int(sha3_512.hashBinaryStringToHex(sha3_512.h2b(str(item_to_hash).encode("utf-8").hex())), 16)
+        self.hash = self.sha3.hashStringToHex(item_to_hash)
+        if self.is_debug:
+            print(self.hash)
+        return self.hexStringToBitString(self.hash)
     
-    def createSignature(self, message):
+    def bitStringToHexString(self, bit_string:str) -> str :
         '''
-        This method creates the signature (R,s) for a message
+        This method translates a bit string into a hex string 
+
+        Parameters :
+            bit_string : str
+                The bit string to be translated
+
+        Returns :
+            hex_string : str
+                The hex string equivalent to the bit sting
+        '''
+
+        bit_string = bit_string.replace(" ","")
+        bit_len = len(bit_string)
+        value = int(bit_string,2)
+        hex_string = '{0:0{1}x}'.format(value,bit_len//4).upper()
+        return hex_string
+
+    def  hexStringToBitString(self, hex_string:str) -> str:
+        '''
+        This method translates a hex string into a bit string 
+
+        Parameters :
+            hex_string : str
+                The hex_string to be translated
+
+        Returns :
+            bit_string : str
+                The bit string equivalent to the hex sting
+        '''
+
+        hex_string = hex_string.replace(" ","")
+        hex_len = len(hex_string)
+        value = int(hex_string,16)
+        bit_string = '{0:0{1}b}'.format(value,hex_len*4)
+        return bit_string
+    
+    def generateSecretK(self) -> int:
+        '''
+        This method generates a 1 time secret value for creating a signature that is less than n
+        '''
+        return secrets.randbelow(self.n)
+    
+    def multiplesOfG(self, multiplier:int) -> tuple[int,int]:
+        '''
+        This method multiplies the generator point by a constant
+
+        Parameters :
+            multiplier : int
+                The constant by which to multiply
+
+        Returns : 
+            result : (int, int)
+                The multiplied point
+        '''
+        return self.curve.calculatedPointMultiplicationByConstant_doubleAndAddMethod(self.curve_details.generator_point,multiplier)
+    
+    def createSignature(self, message_string:str , d:int = None, k:int = None, is_debug:bool = False) -> tuple[str,str]:
+        '''
+        This method creates the signature (r,s) for a message
 
         Parameters : 
             message : str
-                The message as an integer
+                The message as a string
+            d : int
+                The private key to be used, default is your own
+            k : int 
+                The one time variable to be used, defaults to random generation
+            is_debug : Boolean
+                Whether the method is being debugged and should save intermediate values
 
         Returns :
-            R : (int, int)
-                A point which is half of the signature
-            s : int
-                An integer which is half of the signature
+            r : str
+                The x coordinate of a point which is half of the signature as a hexadecimal string
+            s : str
+                An integer which is half of the signature as a hexadecimal string
+
+        implemented based on Nist Fips 168-5 Section 6.4.1 "ECDSA Signature Generation Algorithm"
+        https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-5.pdf
+        '''
+        
+        bit_hash = self.calculateHashOfItem(message_string)
+        hash_length = len(bit_hash)
+        if hash_length > self.length_n:
+            E = bit_hash[:self.length_n]
+        else:
+            E = bit_hash
+        if d == None:
+            d = self.private_key
+        e = self.bitStringToInt(E)
+        r = 0
+        s = 0
+        while r == 0 or s == 0:
+            if k == None:
+                k = self.generateSecretK()
+            k_inv = calculateModuloInverse(k,self.n)
+            R = self.multiplesOfG(k)
+            R_x, R_y = R
+            r_1 = self.curve.convertFieldElementToInt(R_x)
+            r = r_1 % self.n
+            s = k_inv * ((e + (r * d))  % self.n)  % self.n
+            if r == 0 or s == 0:
+                k = self.generateSecretK()
+        if is_debug or self.is_debug:
+            self.H = self.bitStringToHexString(bit_hash)
+            self.E = self.bitStringToHexString(E)
+            self.K = self.intToHexString(k)
+            self.k_inv = self.intToHexString(k_inv)
+            self.R_x = self.intToHexString(R_x)
+            self.R_y = self.intToHexString(R_y)
+            self.r = self.intToHexString(r)
+            self.d = self.intToHexString(d)
+            self.s = self.intToHexString(s)
+            print("Signature Has Been Succesfully Generated")
+            print(f"r: {self.r}")
+            print(f"s: {self.s}")
+        return (self.intToHexString(r), self.intToHexString(s))
+    
+    def intToHexString(self, integer_value:int)-> str:
+        return self.bitStringToHexString(self.intToBitString(integer_value))
+    
+    def bitStringToInt(self, bit_string:str) -> int:
+        '''
+        This method converts a bit string to an integer value
+
+        Parameters : 
+            bit_string : str
+                The string of the bits to be converted
+
+        Returns :
+            integer_value : int
+                The integer representing the converted bit string
+
+        follows the algorithm from Nist FIPS 186.5 B.2.1 "Conversion of a Bit String to an Integer"
+        https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-5.pdf
+        '''
+        integer_value = 0
+        length = len(bit_string)
+        for i in range(0, length):
+            integer_value += int(bit_string[i:i+1],2)*(2**(length-1-i))
+        return integer_value
+    
+    def intToBitString(self, int_value:int) -> str:
+        '''
+        This method converts an integer value to a bit string
+
+        Parameters : 
+            integer_value : int
+                The integer representing the converted bit string
+            
+        Returns :
+            bit_string : str
+                The string of the bits equal to the original value
+
+        follows the algorithm from Nist FIPS 186.5 B.2.1 "Conversion of a Bit String to an Integer"
+        https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-5.pdf
+        '''
+        current_int = int_value
+        bit_string = ""
+        while current_int > 0:
+            next_bit = current_int % 2
+            bit_string = str(next_bit)+bit_string
+            current_int //= 2
+        return bit_string
+    
+    def intToOctetList(self, int_value:int)->list[int]:
+        '''
+        This method converts an integer value to an octet list
+        Parameters : 
+            integer_value : int
+                The integer representing the converted bit string
+            
+        Returns :
+            octet_list : str
+                The octet list equal to the original value
+
+        follows the algorithm from Nist FIPS 186-5 B.2.3 "Conversion of an Integer to an Octet String"
+        https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-5.pdf
         '''
 
-        message_as_int = self.convertMessageToInt(message=message)
-
-        r = self.calculateHashOfItem(self.calculateHashOfItem(message_as_int) + message_as_int) % self.curve.finite_field
-        R = self.curve.calculatedPointMultiplicationByConstant_doubleAndAddMethod(self.curve_details.generator_point,r)
-        h = self.calculateHashOfItem(R[0] + self.public_key[0] + message_as_int) % self.curve.finite_field
-        s = (r + h * self.private_key) 
-
-        if self.is_debug:
-            print("The signature has been generated")
-            print(f"R: {R}")
-            print(f"s: {s}")
-        return (R,s)
+        current_int = int_value
+        octet_list = []
+        while current_int > 0:
+            next_octet = current_int % 256
+            octet_list.insert(0, next_octet)
+            current_int //= 256
+        return octet_list
     
-    def verifySignature(self, message, signature):
+    def octetListToInt(self, octet_list:list[int]) -> int:
+        '''
+        This method converts an octet list to an integer value
+
+        Parameters : 
+            octet_list : list[int]
+                The list of octets to be converted
+
+        Returns :
+            integer_value : int
+                The integer representing the converted octet list
+
+        reversal of the algorithm from Nist 186-5 B.2.3 "Conversion of an Integer to an Octet String"
+        https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-5.pdf
+        '''
+
+        integer_value = 0
+        length = len(octet_list)
+        for i in range(0, length):
+            integer_value += octet_list[i]*(256**(length-1-i))
+        return integer_value
+    
+    def bitStringToOctetList(self, bit_string:str, modulo:int)->list[int]:
+        '''
+        This method converts a bit string to an octet list
+
+        Parameters : 
+            bit_string : str
+                The bit string to be converted
+            modulo : int
+                The modulus value for the int
+
+        Returns :
+            octet_list : [int]
+                The octet list representing the bit string
+
+        uses the algorithm from Nist 186-5 B.2.4 "Conversion of a Bit String to an Octet String"
+        https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-5.pdf
+        '''
+
+        length = len(bit_string)
+        length_modulo = len(self.intToBitString(modulo))
+        if length < length_modulo:
+            bit_string = (length_modulo - length)*"0" + bit_string
+        elif length > length_modulo:
+            bit_string = bit_string[:length_modulo]
+
+        integer_value = self.bitStringToInt(bit_string)
+        if integer_value > modulo:
+            integer_value = integer_value % modulo
+        X = self.intToOctetList(integer_value)
+        return X
+    
+    def hexStringToInt(self, hex_string:str)-> int:
+        '''
+        This method converts a hex string to an int
+
+        Parameters : 
+            hex_string : str
+                The hex string to be converted
+
+        Returns :
+            int_value : int
+                The hex strings value as an integer
+        '''
+
+        return self.bitStringToInt(self.hexStringToBitString(hex_string))
+
+    def verifySignature(self, message_string, signature, public_key = None, is_debug = False):
         '''
         This method verifies the signature using the public key, message and signature
 
         Parameters :
-            message : int
+            message_string : int
                 The message as that was sent with the signature
-            signature : ((int, int), int)
-                The message signature sent as a point and an integer
+            signature : (str, str)
+                The message signature sent as two strings
+            public_key : int, optional
+                The public key for the signature, default uses your own public key
+            is_debug : Bool, optional
+                Whether the method is being debuggued and should store intermediate values
+
+        Returns : 
+            is_verified : boolean
+                Whether the signature was successfully verified
+
+        implemented based on nist fips 186-5 section 6.4.2 "ECDSA Signature Verification Algorithm"
+        https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-5.pdf
         '''
 
-        R,s = signature
-
-        message_as_int = self.convertMessageToInt(message=message)
-
-        hash = self.calculateHashOfItem(R[0] + self.public_key[0] + message_as_int) % self.curve.finite_field
-        signature_verification_point = self.curve.calculatedPointMultiplicationByConstant_doubleAndAddMethod(self.curve_details.generator_point,s) 
-        point_with_hash = self.curve.calculatedPointMultiplicationByConstant_doubleAndAddMethod(self.public_key, hash)
-        signature_verification_point_two = self.curve.calculatePointAddition(R, point_with_hash)
-        if signature_verification_point[0] == signature_verification_point_two[0] and signature_verification_point[1] == signature_verification_point_two[1]:
-            if self.is_debug:
-                print("The signature has been successfully verified")
-            return True
+        if public_key == None:
+            Q = self.public_key
         else:
-            if self.is_debug:
-                print("The signature has failed verification")
-                print(f"Signature Verification Point 1 : {signature_verification_point}")
-                print(f"Signature Verification Point 2 : {signature_verification_point_two}")
-
+            Q = public_key
+        r, s = signature
+        try:
+            if type(r) != int:
+                r = self.hexStringToInt(r)
+            if type(s) != int:
+                s = self.hexStringToInt(s)
+            if type(Q[0]) != int:
+                Q = (self.hexStringToInt(Q[0]), Q[1])
+            if type(Q[1]) != int:
+                Q = (Q[0], self.hexStringToInt(Q[1]))
+        except:
             return False
-        
-    def convertMessageToInt(self, message):
-        '''
-        This method translates a message into an integer value
-        '''
+        bit_hash = self.calculateHashOfItem(message_string)
+        hash_length = len(bit_hash)
+        if hash_length > self.length_n:
+            E = bit_hash[:self.length_n]
+        else:
+            E = bit_hash
+        e = self.bitStringToInt(E)
+        s_inv = calculateModuloInverse(s,self.n)
+        u = e * s_inv % self.n
+        v = r * s_inv % self.n
 
-        message_utf8 = message.encode('utf-8')
-        message_in_hex = message_utf8.hex()
-        message_as_int = int(message_in_hex, 16)
-        return message_as_int
+        uG = self.multiplesOfG(u)
+        vQ = self.curve.calculatedPointMultiplicationByConstant_doubleAndAddMethod(Q, v)
+        R_1 = self.curve.calculatePointAddition(uG, vQ)
+        if R_1 == (0,0): return False
+        R1_x, R1_y = R_1
+        r_1 = self.curve.convertFieldElementToInt(R1_x)
 
-if __name__ == '__main__':
-    
+        if is_debug or self.is_debug:
+            self.H = self.bitStringToHexString(bit_hash)
+            self.E = self.bitStringToHexString(E)
+            self.Q_x = self.intToHexString(Q[0])
+            self.Q_y = self.intToHexString(Q[1])
+            self.sinv = self.intToHexString(s_inv)
+            self.u = self.intToHexString(u)
+            self.v = self.intToHexString(v)
+            self.R1_x = self.intToHexString(R1_x)
+            self.R1_y = self.intToHexString(R1_y)
+            self.r_1 = self.intToHexString(r_1)
+            self.r = self.intToHexString(r)
+            self.s = self.intToHexString(s)
+            self.verified = (r == r_1 % self.n)
+            if self.verified:
+                print("The Signature Has Successfully Been Verified")
+            else:
+                print("The Signature Failed Validation")
+        if r == r_1 % self.n: return True
+        else: return False
+       
+if __name__ == '__main__':    
     print("The example runs the elliptic curve digital signature algorithm for a given message and verifies the signature")
     print("The Elliptic Curve math is based on Weirstrass form elliptic curves and implemented in HelperFunctions.EllipticCurveCalculations")
     
     print("- - - - - - - - - - - -")
 
-    elliptic_curve_digital_signature_algorithm = EllipticCurveDigitalSignatureAlgorithm([EllipticCurveDetails.getCurveP192,EllipticCurveDetails.getSecp256r1,EllipticCurveDetails.getCurveP521],is_debug=True)
+    elliptic_curve_digital_signature_algorithm = EllipticCurveDigitalSignatureAlgorithm([EllipticCurveDetails.getCurveP192,EllipticCurveDetails.getCurveP224,EllipticCurveDetails.getCurveP521,EllipticCurveDetails.getSecp256r1],is_debug=True)
  
     print("- - - - - - - - - - - -")
     
     message = "This is the message which is being signed"
-    signature = elliptic_curve_digital_signature_algorithm.createSignature(message=message)
+    signature = elliptic_curve_digital_signature_algorithm.createSignature(message)
     
     print("- - - - - - - - - - - -")
     is_signature_valid = elliptic_curve_digital_signature_algorithm.verifySignature(message,signature)
-
-    assert is_signature_valid
     
+    assert is_signature_valid
