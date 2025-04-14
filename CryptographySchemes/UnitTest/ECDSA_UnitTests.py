a0dc3e1e9da32e55c54b9c1aee779b08be5331e3
Jamie <jbarbourmoore@gmail.com>
2025-04-14 14:39:13 -0400

    Update ECDSA_UnitTests.py

--- CryptographySchemes/UnitTest/ECDSA_UnitTests.py
+++ CryptographySchemes/UnitTest/ECDSA_UnitTests.py
@@ -22,21 +22,29 @@ class ECDSA_UnitTests(unittest.TestCase):
         self.R = "140C8EDCA57108CE3F7E7A240DDD3AD74D81E2DE62451FC1D558FDC79269ADACD1C2526EEEEF32F8C0432A9D56E2B4A8A732891C37C9B96641A9254CCFE5DC3E2BA"
         self.D = "100085F47B8E1B8B11B7EB33028C0B2888E304BFC98501955B45BBA1478DC184EEEDF09B86A5F7C21994406072787205E69A63709FE35AA93BA333514B24F961722"
         self.S = "B25188492D58E808EDEBD7BF440ED20DB771CA7C618595D5398E1B1C0098E300D8C803EC69EC5F46C84FC61967A302D366C627FCFA56F87F241EF921B6E627ADBF"
+
+        self.k_int = self.ecdsa.bitStringToInt(self.ecdsa.format_hex_as_bit_string(self.K))
+        self.d_int = self.ecdsa.bitStringToInt(self.ecdsa.format_hex_as_bit_string(self.D))
     
+    def generate_signature_p521_sha3512(self):
+        self.ecdsa.generateSignature(self.message,self.d_int,self.k_int,True)
+
     def test_hash_as_H(self):
         '''
         This method tests the sha3-512 hashing generates the expected hex value
         '''
+
         print("Testing SHA2-512 Hashing")
         actual_hash = self.sha.hashStringToHex(self.message)
         print(f"Actual Hash   : {actual_hash}")
-        print(f"Expected Hash : {actual_hash}")
+        print(f"Expected Hash : {self.H}")
         self.assertEqual(self.H,actual_hash)
 
     def test_bit_to_int(self):
         '''
         This method tests the bit string to int method is working
         '''
+
         bit_string = "1011101110"
         expected_result = 750
         int_value = self.ecdsa.bitStringToInt(bit_string=bit_string)
@@ -48,18 +56,102 @@ class ECDSA_UnitTests(unittest.TestCase):
         '''
         This method tests the int to bit string method is working
         '''
+
         expected_result = "1011101110"
         int_value = 750
         bit_string = self.ecdsa.intToBitString(int_value)
         print("Test Int To Bit String")
         print(f"{int_value} -> {bit_string} ?= expected:{expected_result}")
         self.assertEqual(bit_string, expected_result)
+        
     def test_E(self):
-        #If len(n) ≥ hashlen, set E = H. Otherwise, set E equal to the leftmost log2(n) bits of H.
-        pass
+        '''
+        This method tests that the E value matches the known value from NIST
+        https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/P521_SHA3-512.pdf
+        '''
+
+        self.generate_signature_p521_sha3512()
+        print("Checking the Value for E When Generating Signature")
+        print(f"Actual E      : {self.ecdsa.E}")
+        print(f"Expected E    : {self.E}")
+        self.assertEqual(self.ecdsa.E,self.E)
+
+    def test_K(self):
+        '''
+        This method tests that the K value matches the known value from NIST
+        https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/P521_SHA3-512.pdf
+        '''
+        self.generate_signature_p521_sha3512()
+        print("Checking the Value for K When Generating Signature")
+        print(f"Actual K      : {self.ecdsa.K}")
+        print(f"Expected K    : {self.K}")
+        self.assertEqual(self.ecdsa.K,self.K)
 
     def test_K_inv(self):
-        pass
+        '''
+        This method tests that the K_inv value matches the known value from NIST
+        https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/P521_SHA3-512.pdf
+        '''
+        self.generate_signature_p521_sha3512()
+        print("Checking the Value for K Inverse When Generating Signature")
+        print(f"Actual Kinv   : {self.ecdsa.k_inv}")
+        print(f"Expected Kinv : {self.Kinv}")
+        self.assertEqual(self.ecdsa.k_inv,self.Kinv)
+
+    def test_R_x(self):
+        '''
+        This method tests that the R_x value matches the known value from NIST
+        https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/P521_SHA3-512.pdf
+        '''
+        self.generate_signature_p521_sha3512()
+        print("Checking the Value for R_x When Generating Signature")
+        print(f"Actual R_x    : {self.ecdsa.R_x}")
+        print(f"Expected R_x  : {self.R_x}")
+        self.assertEqual(self.ecdsa.R_x,self.R_x)
+
+    def test_R_y(self):
+        '''
+        This method tests that the R_y value matches the known value from NIST
+        https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/P521_SHA3-512.pdf
+        '''
+        self.generate_signature_p521_sha3512()
+        print("Checking the Value for R_y When Generating Signature")
+        print(f"Actual R_y    : {self.ecdsa.R_y}")
+        print(f"Expected R_y  : {self.R_y}")
+        self.assertEqual(self.ecdsa.R_y,self.R_y)
+    
+    def test_r(self):
+        '''
+        This method tests that the r value matches the known value from NIST
+        https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/P521_SHA3-512.pdf
+        '''
+        self.generate_signature_p521_sha3512()
+        print("Checking the Value for r When Generating Signature")
+        print(f"Actual r      : {self.ecdsa.r}")
+        print(f"Expected r    : {self.R}")
+        self.assertEqual(self.ecdsa.r,self.R)
+
+    def test_D(self):
+        '''
+        This method tests that the D value matches the known value from NIST
+        https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/P521_SHA3-512.pdf
+        '''
+        self.generate_signature_p521_sha3512()
+        print("Checking the Value for D When Generating Signature")
+        print(f"Actual D      : {self.ecdsa.d}")
+        print(f"Expected D    : {self.D}")
+        self.assertEqual(self.ecdsa.d,self.D)
+
+    def test_s(self):
+        '''
+        This method tests that the s value matches the known value from NIST
+        https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/P521_SHA3-512.pdf
+        '''
+        self.generate_signature_p521_sha3512()
+        print("Checking the Value for s When Generating Signature")
+        print(f"Actual s      : {self.ecdsa.s}")
+        print(f"Expected s    : {self.S}")
+        self.assertEqual(self.ecdsa.s,self.S)
 
 if __name__ == '__main__':
     print("Curve: P-521")
