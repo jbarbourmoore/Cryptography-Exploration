e659995af46a0cb3d1d22d0bbd46883ef0500c67
Jamie <jbarbourmoore@gmail.com>
2025-04-26 18:36:13 -0400

    TDES CFB Tests With different S Values

--- CryptographySchemes/UnitTest/TDES_UnitTests.py
+++ CryptographySchemes/UnitTest/TDES_UnitTests.py
@@ -104,12 +104,12 @@ class TDES_UnitTest(unittest.TestCase):
         tdes = TDES_CFB(key=key, s=s, is_hex_key=True)
         result_hex = tdes.encryptHexString(hex_to_encrypt,initialization_vector=initialization_vector)
         self.assertEqual(expected_results, result_hex)
-        print("Testing Encryption With Triple Data Encryption Standard In Cypher Feedback (CFB) Mode")
+        print("Testing Encryption With Triple Data Encryption Standard In Cypher Feedback (CFB) Mode With 64 Bits")
         print(f"Plain Text           : {hex_to_encrypt}")
         print(f"Expected Cypher Text : {expected_results}")
         print(f"Encrypted Text       : {result_hex}")
 
-    def test_tdes_cfb_64_2decrypt(self):
+    def test_tdes_cfb_s64_2decrypt(self):
         '''
         This method tests that the tdes decryption works properly in Cypher Feedback (CFB) mode
         according to https://github.com/coruus/nist-testvectors/blob/master/csrc.nist.gov/groups/ST/toolkit/documents/Examples/TDES_ModesA_All.txt
@@ -125,14 +125,96 @@ class TDES_UnitTest(unittest.TestCase):
         tdes = TDES_CFB(key=key, s=s, is_hex_key=True)
         result_hex = tdes.decryptHexString(hex_to_decrypt, initialization_vector)
         self.assertEqual(expected_results, result_hex)
-        print("Testing Decryption With Triple Data Encryption Standard In Cypher Feedback (CFB) Mode")
+        print("Testing Decryption With Triple Data Encryption Standard In Cypher Feedback (CFB) Mode With 64 Bits")
+        print(f"Cypher Text          : {hex_to_decrypt}")
+        print(f"Expected Plain Text  : {expected_results}")
+        print(f"Decrypted Text       : {result_hex}")
+
+    def test_tdes_cfb_s1_1encrypt(self):
+        '''
+        This method tests that the tdes encryption works properly in Cypher Feedback (CFB) mode 
+        according to https://github.com/coruus/nist-testvectors/blob/master/csrc.nist.gov/groups/ST/toolkit/documents/Examples/TDES_ModesA_All.txt
+        '''
+
+        hex_to_encrypt   =  "6BC1BEE2 2E409F96 E93D7E11 7393172A AE2D8A57 1E03AC9C 9EB76FAC 45AF8E51".replace(" ","")
+        expected_results =  "5CB0126B 0CBD982E 68BCDEFD A8055647 85766DAC DF059F88 0E47F037 F69E9418".replace(" ","")
+        
+        key = "01234567 89ABCDEF 23456789 ABCDEF01 456789AB CDEF0123".replace(" ","")
+        initialization_vector = "F69F2445DF4F9B17"
+        s = 1
+
+        tdes = TDES_CFB(key=key, s=s, is_hex_key=True)
+        result_hex = tdes.encryptHexString(hex_to_encrypt,initialization_vector=initialization_vector)
+        self.assertEqual(expected_results, result_hex)
+        print("Testing Encryption With Triple Data Encryption Standard In Cypher Feedback (CFB) Mode With 1 Bit")
+        print(f"Plain Text           : {hex_to_encrypt}")
+        print(f"Expected Cypher Text : {expected_results}")
+        print(f"Encrypted Text       : {result_hex}")
+
+    def test_tdes_cfb_s1_2decrypt(self):
+        '''
+        This method tests that the tdes decryption works properly in Cypher Feedback (CFB) mode
+        according to https://github.com/coruus/nist-testvectors/blob/master/csrc.nist.gov/groups/ST/toolkit/documents/Examples/TDES_ModesA_All.txt
+        '''
+
+        expected_results = "6BC1BEE2 2E409F96 E93D7E11 7393172A AE2D8A57 1E03AC9C 9EB76FAC 45AF8E51".replace(" ","")
+        hex_to_decrypt   = "5CB0126B 0CBD982E 68BCDEFD A8055647 85766DAC DF059F88 0E47F037 F69E9418".replace(" ","")
+        
+        key = "01234567 89ABCDEF 23456789 ABCDEF01 456789AB CDEF0123".replace(" ","")
+        initialization_vector = "F69F2445DF4F9B17"
+        s = 1
+
+        tdes = TDES_CFB(key=key, s=s, is_hex_key=True)
+        result_hex = tdes.decryptHexString(hex_to_decrypt, initialization_vector)
+        self.assertEqual(expected_results, result_hex)
+        print("Testing Decryption With Triple Data Encryption Standard In Cypher Feedback (CFB) Mode With 1 Bit")
+        print(f"Cypher Text          : {hex_to_decrypt}")
+        print(f"Expected Plain Text  : {expected_results}")
+        print(f"Decrypted Text       : {result_hex}")
+
+    def test_tdes_cfb_s8_1encrypt(self):
+        '''
+        This method tests that the tdes encryption works properly in Cypher Feedback (CFB) mode 
+        according to https://github.com/coruus/nist-testvectors/blob/master/csrc.nist.gov/groups/ST/toolkit/documents/Examples/TDES_ModesA_All.txt
+        '''
+
+        hex_to_encrypt   =  "6BC1BEE2 2E409F96 E93D7E11 7393172A AE2D8A57 1E03AC9C 9EB76FAC 45AF8E51".replace(" ","")
+        expected_results =  "07951B72 9DC23AB4 48FC82B4 0372623D C443A4B4 43B6B4A6 6C20D892 236028D5".replace(" ","")
+        
+        key = "01234567 89ABCDEF 23456789 ABCDEF01 456789AB CDEF0123".replace(" ","")
+        initialization_vector = "F69F2445DF4F9B17"
+        s = 8
+
+        tdes = TDES_CFB(key=key, s=s, is_hex_key=True)
+        result_hex = tdes.encryptHexString(hex_to_encrypt,initialization_vector=initialization_vector)
+        self.assertEqual(expected_results, result_hex)
+        print("Testing Encryption With Triple Data Encryption Standard In Cypher Feedback (CFB) Mode With 8 Bits")
+        print(f"Plain Text           : {hex_to_encrypt}")
+        print(f"Expected Cypher Text : {expected_results}")
+        print(f"Encrypted Text       : {result_hex}")
+
+    def test_tdes_cfb_s8_2decrypt(self):
+        '''
+        This method tests that the tdes decryption works properly in Cypher Feedback (CFB) mode
+        according to https://github.com/coruus/nist-testvectors/blob/master/csrc.nist.gov/groups/ST/toolkit/documents/Examples/TDES_ModesA_All.txt
+        '''
+
+        expected_results = "6BC1BEE2 2E409F96 E93D7E11 7393172A AE2D8A57 1E03AC9C 9EB76FAC 45AF8E51".replace(" ","")
+        hex_to_decrypt   = "07951B72 9DC23AB4 48FC82B4 0372623D C443A4B4 43B6B4A6 6C20D892 236028D5".replace(" ","")
+        
+        key = "01234567 89ABCDEF 23456789 ABCDEF01 456789AB CDEF0123".replace(" ","")
+        initialization_vector = "F69F2445DF4F9B17"
+        s = 8
+
+        tdes = TDES_CFB(key=key, s=s, is_hex_key=True)
+        result_hex = tdes.decryptHexString(hex_to_decrypt, initialization_vector)
+        self.assertEqual(expected_results, result_hex)
+        print("Testing Decryption With Triple Data Encryption Standard In Cypher Feedback (CFB) Mode With 8 Bits")
         print(f"Cypher Text          : {hex_to_decrypt}")
         print(f"Expected Plain Text  : {expected_results}")
         print(f"Decrypted Text       : {result_hex}")
 
 
-
-    
 if __name__ == '__main__':
     print("Testing TDES With Block Cypher Modes Of Operation")
     print("https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38a.pdf")
