e6bdc94c3be34ac4ef5feda64d8a8648e4bfe19d
Jamie <jbarbourmoore@gmail.com>
2025-04-28 08:23:34 -0400

    Added a second unit test

--- CryptographySchemes/UnitTest/CMAC_UnitTests.py
+++ CryptographySchemes/UnitTest/CMAC_UnitTests.py
@@ -37,6 +37,32 @@ class CMAS_UnitTest(unittest.TestCase):
         print(f"Subkey (2)  : {k2.getHexString()}")
         print(f"Tag         : {tag}")
 
+    def test_3des_2block_message(self):
+        '''
+        This method tests CMAC with the 3DES block cypher and a 2 block message
+
+        CMAC is Laid Out In Nist SP 800-38b : https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-38b.pdf
+        Test vectors from https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/TDES_CMAC.pdf
+        '''
+
+        key = "01234567 89ABCDEF 23456789 ABCDEF01 456789AB CDEF0123".replace(" ","")
+        plain_text = "6BC1BEE2 2E409F96 E93D7E11 7393172A".replace(" ","")
+        tag_length = 64
+
+        expected_tag = "30239CF1 F52E6609".replace(" ","")
+        expected_subkey = "9D74E739 331796C0".replace(" ","")
+
+        cmac = CMAC_3DES(key=key)
+        k1, k2 = cmac.subkeyGeneration()
+        tag = cmac.cmacGeneration(plain_text, tag_length)
+
+        self.assertEqual(k1.getHexString(), expected_subkey)
+        self.assertEqual(tag, expected_tag)
+
+        print("Testing CMAC With 3DES And An A Two Block Long Plain Text")
+        print(f"Message     : \"{plain_text}\"")
+        print(f"Subkey (1)  : {k2.getHexString()}")
+        print(f"Tag         : {tag}")
         
 
 
