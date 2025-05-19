3948c71ccd96654d4f40b71791be3086c74c1562
Jamie <190376747+jbarbourmoore@users.noreply.github.com>
2025-05-19 09:46:40 -0400

    Added new test structure to cmake and refactored xor

--- C++_Implementations/HelperFunctions/src/BigNumXOR.cpp
+++ C++_Implementations/HelperFunctions/src/BigNumXOR.cpp
@@ -7,6 +7,40 @@
 
 #include "BigNumHelpers.hpp"
 
+void BigNumHelpers::xorBigNums(BIGNUM* result_pointer, BIGNUM* first_bn, BIGNUM* second_bn){
+    int first_num_bytes = BN_num_bytes(first_bn);
+    int second_num_bytes = BN_num_bytes(second_bn);
+
+    // Find the maximum byte length of the two inputs
+    int max_bytes = first_num_bytes;
+    if (second_num_bytes > max_bytes){
+        max_bytes = second_num_bytes;
+    }
+    
+    // Create two character arrays of the maximum byte length and populate them with the inputes (padded if necessary)
+    unsigned char first_char_array[max_bytes];
+    unsigned char second_char_array[max_bytes];
+    if (first_num_bytes == second_num_bytes) {
+        BN_bn2bin(first_bn, first_char_array);
+        BN_bn2bin(second_bn, second_char_array);
+    } else if (first_num_bytes > second_num_bytes) {
+        BN_bn2bin(first_bn, first_char_array);
+        BN_bn2binpad(second_bn, second_char_array, max_bytes);
+    } else {
+        BN_bn2binpad(first_bn, first_char_array, max_bytes);
+        BN_bn2bin(second_bn, second_char_array);
+    }
+
+    // Perform the XOR of each Byte
+    unsigned char result_char_array[max_bytes];
+    for(int i = 0; i < max_bytes; ++i) {
+        result_char_array[i] = first_char_array[i] ^ second_char_array[i];
+    }
+
+    // Transform the result into a new BIGNUM and put it in the BIGNUM result is pointing too
+    BN_bin2bn(result_char_array, max_bytes, result_pointer);
+}
+
 BIGNUM* BigNumHelpers::xorBigNums(BIGNUM* first_bn, BIGNUM* second_bn){
     int first_num_bytes = BN_num_bytes(first_bn);
     int second_num_bytes = BN_num_bytes(second_bn);
