30c54bb58d2dc95ebc533d4308e1a53c51cf84a0
Jamie <190376747+jbarbourmoore@users.noreply.github.com>
2025-05-22 14:07:02 -0400

    Added multiplication within galois field 2**8

--- C++_Implementations/AES/include/AES.hpp
+++ C++_Implementations/AES/include/AES.hpp
@@ -1,7 +1,7 @@
 /// @brief This class should include the cypher and most of the necessary components for Advanced Encryption Standard
 /// as laid out in nist fips 197 https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197-upd1.pdf
 class AES{
-    private :
+    protected :
         /// @brief The array of values for substitution from NIST FIPS 197 : Table 4. "SBOX(): substitution values for the byte xy (in hexadecimal format)"
         static const unsigned char SBOX[256];
 
@@ -16,4 +16,10 @@ class AES{
 
         /// @brief The array of values for the inverse mix columns constants from NIST FIPS 197 Section 5.3.3 "InvMixColumns()"
         static const unsigned char INVMIXCOLS[16];
+
+        /// @brief This method performs the multiplication of a byte within galois field 2**8
+        /// @param byte_to_multiply The byte which is being multiplied
+        /// @param mult_factor The factor by which the byte is being multiplied
+        /// @return The result of the multiplication of the byte by the factor in the galois field 2**8
+        static unsigned char xTimes(unsigned char byte_to_multiply, int mult_factor);
 };
--- C++_Implementations/AES/src/AES.cpp
+++ C++_Implementations/AES/src/AES.cpp
@@ -1,1 +1,26 @@
 #include "AES.hpp"
+
+unsigned char AES::xTimes(unsigned char byte_to_multiply, int mult_factor){
+    unsigned char result = 0;
+    if(mult_factor == 1){
+        result = byte_to_multiply;
+    }
+    else if(mult_factor == 2){
+        result = (byte_to_multiply << 1) & 0xff;
+        if(result >= 128){
+            result = result ^ 0x1b;
+        }
+    }
+    else if (mult_factor % 2 == 0){
+        unsigned char x_time_res = xTimes(byte_to_multiply, mult_factor / 2);
+        result = (x_time_res << 1) && 0xff;
+        if(result >= 128){
+            result = result ^ 0x1b;
+        }
+    }
+    else {
+        result = xTimes(byte_to_multiply, mult_factor - 1);
+        result = result ^ byte_to_multiply;
+    }
+    return result;
+}
