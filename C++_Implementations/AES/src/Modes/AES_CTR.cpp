55af956b69c808ed057bd99ea5295e1c1414dd9c
Jamie <190376747+jbarbourmoore@users.noreply.github.com>
2025-05-24 18:27:09 -0400

    Added AES CTR for 192 and 256 bits

--- C++_Implementations/AES/src/Modes/AES_CTR.cpp
+++ C++_Implementations/AES/src/Modes/AES_CTR.cpp
@@ -69,3 +69,144 @@ std::vector<AESDataBlock> AES_CTR::AES128InvCypher(std::vector<AESDataBlock> inp
     std::vector<AESWord> expanded_key = AESKey::keyExpansion(key,AES_KEY_128);
     return AES_CTR::AES128InvCypher(input, expanded_key, initialization_vector);
 }
+
+
+std::vector<AESDataBlock> AES_CTR::AES192Cypher(std::string input, std::vector<AESWord> expanded_key, AESDataBlock initialization_vector){
+    std::vector<AESDataBlock> input_blocks = AESDataBlock::dataBlocksFromHexString(input);
+    std::vector<AESDataBlock> output_blocks = AES_CTR::AES192Cypher(input_blocks, expanded_key, initialization_vector);
+    return output_blocks;
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192Cypher(std::string input, std::string key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key);
+    return AES_CTR::AES192Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192Cypher(std::string input, unsigned char *key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key,AES_KEY_192);
+    return AES_CTR::AES192Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192InvCypher(std::string input, std::vector<AESWord> expanded_key, AESDataBlock initialization_vector){
+    std::vector<AESDataBlock> input_blocks = AESDataBlock::dataBlocksFromHexString(input);
+    std::vector<AESDataBlock> output_blocks = AES_CTR::AES192InvCypher(input_blocks, expanded_key, initialization_vector);
+    return output_blocks;
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192InvCypher(std::string input, std::string key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key);
+    return AES_CTR::AES192InvCypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192InvCypher(std::string input, unsigned char *key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key,AES_KEY_192);
+    return AES_CTR::AES192InvCypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192Cypher(std::vector<AESDataBlock> input, std::vector<AESWord> expanded_key, AESDataBlock initialization_vector){
+    std::vector<AESDataBlock> output_blocks;
+    AESDataBlock T = AESDataBlock(initialization_vector);
+    for (int i = 0; i < input.size(); i ++){
+        AESDataBlock P = AESDataBlock(input.at(i));
+        AESDataBlock O = AES::AES192Cypher(T, expanded_key);
+        AESDataBlock C = AESDataBlock(P);
+        C.xorBlock(O);
+        T.increment(1);
+        output_blocks.push_back(C);
+    }
+    return output_blocks;
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192Cypher(std::vector<AESDataBlock> input, std::string key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key);
+    return AES_CTR::AES192Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192Cypher(std::vector<AESDataBlock> input, unsigned char *key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key,AES_KEY_192);
+    return AES_CTR::AES192Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192InvCypher(std::vector<AESDataBlock> input, std::vector<AESWord> expanded_key, AESDataBlock initialization_vector){
+    return AES192Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192InvCypher(std::vector<AESDataBlock> input, std::string key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key);
+    return AES_CTR::AES192InvCypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES192InvCypher(std::vector<AESDataBlock> input, unsigned char *key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key,AES_KEY_192);
+    return AES_CTR::AES192InvCypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256Cypher(std::string input, std::vector<AESWord> expanded_key, AESDataBlock initialization_vector){
+    std::vector<AESDataBlock> input_blocks = AESDataBlock::dataBlocksFromHexString(input);
+    std::vector<AESDataBlock> output_blocks = AES_CTR::AES256Cypher(input_blocks, expanded_key, initialization_vector);
+    return output_blocks;
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256Cypher(std::string input, std::string key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key);
+    return AES_CTR::AES256Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256Cypher(std::string input, unsigned char *key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key,AES_KEY_256);
+    return AES_CTR::AES256Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256InvCypher(std::string input, std::vector<AESWord> expanded_key, AESDataBlock initialization_vector){
+    std::vector<AESDataBlock> input_blocks = AESDataBlock::dataBlocksFromHexString(input);
+    std::vector<AESDataBlock> output_blocks = AES_CTR::AES256InvCypher(input_blocks, expanded_key, initialization_vector);
+    return output_blocks;
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256InvCypher(std::string input, std::string key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key);
+    return AES_CTR::AES256InvCypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256InvCypher(std::string input, unsigned char *key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key,AES_KEY_256);
+    return AES_CTR::AES256InvCypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256Cypher(std::vector<AESDataBlock> input, std::vector<AESWord> expanded_key, AESDataBlock initialization_vector){
+    std::vector<AESDataBlock> output_blocks;
+    AESDataBlock T = AESDataBlock(initialization_vector);
+    for (int i = 0; i < input.size(); i ++){
+        AESDataBlock P = AESDataBlock(input.at(i));
+        AESDataBlock O = AES::AES256Cypher(T, expanded_key);
+        AESDataBlock C = AESDataBlock(P);
+        C.xorBlock(O);
+        T.increment(1);
+        output_blocks.push_back(C);
+    }
+    return output_blocks;
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256Cypher(std::vector<AESDataBlock> input, std::string key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key);
+    return AES_CTR::AES256Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256Cypher(std::vector<AESDataBlock> input, unsigned char *key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key,AES_KEY_256);
+    return AES_CTR::AES256Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256InvCypher(std::vector<AESDataBlock> input, std::vector<AESWord> expanded_key, AESDataBlock initialization_vector){
+    return AES256Cypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256InvCypher(std::vector<AESDataBlock> input, std::string key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key);
+    return AES_CTR::AES256InvCypher(input, expanded_key, initialization_vector);
+}
+
+std::vector<AESDataBlock> AES_CTR::AES256InvCypher(std::vector<AESDataBlock> input, unsigned char *key, AESDataBlock initialization_vector){
+    std::vector<AESWord> expanded_key = AESKey::keyExpansion(key, AES_KEY_256);
+    return AES_CTR::AES256InvCypher(input, expanded_key, initialization_vector);
+}
--- C++_Implementations/Tests/AESTests/src/ModesTests/AES_CTRTests.cpp
+++ C++_Implementations/Tests/AESTests/src/ModesTests/AES_CTRTests.cpp
@@ -27,3 +27,52 @@ TEST(AES_CTR_Tests, aes128_ctr_test) {
     EXPECT_EQ(actual_cypher_text, given_cypher_text);
     EXPECT_EQ(actual_plain_text, given_plain_text);
 }
+
+
+TEST(AES_CTR_Tests, aes192_ctr_test) {
+    std::string given_plain_string = "6BC1BEE22E409F96E93D7E117393172AAE2D8A571E03AC9C9EB76FAC45AF8E5130C81C46A35CE411E5FBC1191A0A52EFF69F2445DF4F9B17AD2B417BE66C3710";
+    std::string given_cypher_string = "1ABC932417521CA24F2B0459FE7E6E0B090339EC0AA6FAEFD5CCC2C6F4CE8E941E36B26BD1EBC670D1BD1D665620ABF74F78A7F6D29809585A97DAEC58C6B050";
+    std::string initialization_vector = "f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+    unsigned char key_192[24] = {0x8e ,0x73 ,0xb0 ,0xf7 ,0xda ,0x0e, 0x64, 0x52, 0xc8, 0x10, 0xf3, 0x2b, 0x80, 0x90, 0x79, 0xe5, 0x62, 0xf8, 0xea, 0xd2, 0x52, 0x2c, 0x6b, 0x7b};
+    
+    std::vector<AESDataBlock> given_plain_text = AESDataBlock::dataBlocksFromHexString(given_plain_string);
+    std::vector<AESDataBlock> given_cypher_text = AESDataBlock::dataBlocksFromHexString(given_cypher_string);
+
+    printf("\nCypher Result With AES 192\n");
+    std::vector<AESDataBlock> actual_cypher_text = AES_CTR::AES192Cypher(given_plain_string, key_192, initialization_vector);
+    for(int i = 0 ; i < actual_cypher_text.size() ; i ++){
+        actual_cypher_text.at(i).print();
+    }
+    printf("Inverse Cypher Result With AES 192\n");
+    std::vector<AESDataBlock> actual_plain_text = AES_CTR::AES192InvCypher(actual_cypher_text, key_192, initialization_vector);
+    for(int i = 0 ; i < actual_plain_text.size() ; i ++){
+        actual_plain_text.at(i).print();
+    }
+
+    EXPECT_EQ(actual_cypher_text, given_cypher_text);
+    EXPECT_EQ(actual_plain_text, given_plain_text);
+}
+
+TEST(AES_CTR_Tests, aes256_ctr_test) {
+    std::string given_plain_string = "6BC1BEE22E409F96E93D7E117393172AAE2D8A571E03AC9C9EB76FAC45AF8E5130C81C46A35CE411E5FBC1191A0A52EFF69F2445DF4F9B17AD2B417BE66C3710";
+    std::string given_cypher_string = "601EC313775789A5B7A7F504BBF3D228F443E3CA4D62B59ACA84E990CACAF5C52B0930DAA23DE94CE87017BA2D84988DDFC9C58DB67AADA613C2DD08457941A6";
+    std::string initialization_vector = "f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+    unsigned char key_256[32] = {0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b, 0x73, 0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81, 0x1f, 0x35, 0x2c, 0x07, 0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14, 0xdf, 0xf4};
+    
+    std::vector<AESDataBlock> given_plain_text = AESDataBlock::dataBlocksFromHexString(given_plain_string);
+    std::vector<AESDataBlock> given_cypher_text = AESDataBlock::dataBlocksFromHexString(given_cypher_string);
+
+    printf("\nCypher Result With AES 256\n");
+    std::vector<AESDataBlock> actual_cypher_text = AES_CTR::AES256Cypher(given_plain_string, key_256, initialization_vector);
+    for(int i = 0 ; i < actual_cypher_text.size() ; i ++){
+        actual_cypher_text.at(i).print();
+    }
+    printf("Inverse Cypher Result With AES 256\n");
+    std::vector<AESDataBlock> actual_plain_text = AES_CTR::AES256InvCypher(actual_cypher_text, key_256, initialization_vector);
+    for(int i = 0 ; i < actual_plain_text.size() ; i ++){
+        actual_plain_text.at(i).print();
+    }
+
+    EXPECT_EQ(actual_cypher_text, given_cypher_text);
+    EXPECT_EQ(actual_plain_text, given_plain_text);
+}
