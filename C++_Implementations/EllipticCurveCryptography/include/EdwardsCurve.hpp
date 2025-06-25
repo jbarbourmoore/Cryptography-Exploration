935d93af5308f2919e853688f9231363f5818c16
Jamie <190376747+jbarbourmoore@users.noreply.github.com>
2025-06-25 17:55:27 -0400

    Began on edwards 25519

---
+++ C++_Implementations/EllipticCurveCryptography/include/EdwardsCurve.hpp
@@ -0,0 +1,93 @@
+#include "EllipticCurve.hpp"
+
+class EdwardsCurve : public EllipticCurve{
+    private:
+
+        /// @brief The coefficient (a) for x^2 in the equation ax^2 + y^2 = 1 + dx^2y^2
+        BIGNUM* a_;
+
+        /// @brief The coefficient (d) for x^2y^2 in the equation ax^2 + y^2 = 1 + dx^2y^2
+        BIGNUM* d_;
+
+        /// @brief The finite field (p) within which all points are (modulus)
+        BIGNUM* finite_field_;
+
+        /// @brief The origin point (0,0)
+        Point origin_;
+
+        /// @brief The generator point for the curve
+        Point g_;
+
+        /// @brief The order of the curve
+        BIGNUM* n_;
+
+    public:
+
+        /// @brief The default constructor for an Edwards Curve in the form ax^2 + y^2 = 1 + dx^2y^2
+        EdwardsCurve();
+
+        /// @brief The constructor for an Edwards Curve in the form ax^2 + y^2 = 1 + dx^2y^2
+        /// @param a The coefficient (a) for x^2 in the equation ax^2 + y^2 = 1 + dx^2y^2
+        /// @param d The coefficient (d) for x^2y^2 in the equation ax^2 + y^2 = 1 + dx^2y^2
+        /// @param finite_field The finite field (p) within which all points are (modulus)
+        /// @param gx The x coordinate of the generator point for the curve
+        /// @param gy The y coordinate of the generator point for the curve
+        EdwardsCurve(const BIGNUM* a, const BIGNUM* d, const BIGNUM* finite_field, const BIGNUM* gx, const BIGNUM* gy);
+
+        /// @brief The constructor for an Edwards Curve in the form ax^2 + y^2 = 1 + dx^2y^2
+        /// @param a_hex The coefficient (a) for x^2 in the equation ax^2 + y^2 = 1 + dx^2y^2
+        /// @param d_hex The coefficient (d) for x^2y^2 in the equation ax^2 + y^2 = 1 + dx^2y^2
+        /// @param finite_field_hex The finite field (p) within which all points are (modulus)
+        /// @param gx_hex The x coordinate of the generator point for the curve
+        /// @param gy_hex The y coordinate of the generator point for the curve
+        /// @param n_hex The order of the curve
+        EdwardsCurve(std::string a_hex, std::string d_hex, std::string finite_field_hex, std::string gx_hex, std::string gy_hex, std::string n_hex);
+
+        /// @brief This method cleans up the EdwardsCurve and takes care of the BIGNUMs
+        void deleteCurve() override;
+
+        /// @brief This method returns a string containing the the equation for the Edwards curve
+        /// @return The equation as a string value (integers are represented in hexadecimal form)
+        std::string getEquation() override;
+
+        /// @brief This method prints the details of the curve to the console
+        void printCurveDetails() override;
+
+        /// @brief This method validates whether a point exists on the Edwards Curve
+        /// @param p The point that is being checked against the curve
+        /// @return True if the point is on the curve
+        bool validatePointOnCurve(Point p) override;
+
+        /// @brief This method returns the hex string value for the finite field associated with this curve
+        /// @return 
+        std::string getFiniteFieldAsHex();
+
+        /// @brief This method returns the hex string value for the x coefficient associated with this curve
+        /// @return 
+        std::string getAAsHex();
+
+        /// @brief This method returns the hex string value for the constant associated with this curve
+        /// @return 
+        std::string getBAsHex();
+
+        BIGNUM* getN();
+
+        Point getG();
+
+        /// @brief This method multiplies a given point by a constant value
+        /// @param p The point that is to be multiplied by a given constant
+        /// @param k The constant that the point shall be multiplied by
+        /// @return The point resulting from the multiplication
+        Point calculatePointMultiplicationByConstant(Point p, BIGNUM* k) override;
+
+        /// @brief This method calculates the inverse of a given point on the curve
+        /// @param p The point for which we are calculating the inverse
+        /// @return The inverse to the given point
+        Point calculatePointInverse(Point p) override;
+
+        /// @brief This method adds two point together on a given curve
+        /// @param p One of the points to be added together
+        /// @param q One of the points to be added together
+        /// @return The point resulting from the addition
+        Point calculatePointAddition(Point p, Point q) override;
+};
---
+++ C++_Implementations/EllipticCurveCryptography/src/EdwardsCurve.cpp
@@ -0,0 +1,262 @@
+#include "EdwardsCurve.hpp"
+
+EdwardsCurve::EdwardsCurve(){
+    a_ = BN_new();
+    d_ = BN_new();
+    finite_field_ = BN_new();
+    origin_ = Point();
+    g_ = Point();
+}
+
+EdwardsCurve::EdwardsCurve(const BIGNUM* a, const BIGNUM* d, const BIGNUM* finite_field, const BIGNUM* gx, const BIGNUM* gy){
+    a_ = BN_new();
+    BN_copy(a_, a);
+    d_ = BN_new();
+    BN_copy(d_, d);
+    finite_field_ = BN_new();
+    BN_copy(finite_field_, finite_field);
+    origin_ = Point();
+    g_ = Point(gx, gy);
+}
+
+EdwardsCurve::EdwardsCurve(std::string a_hex, std::string d_hex, std::string finite_field_hex, std::string gx_hex, std::string gy_hex, std::string n_hex){
+    a_ = BN_new();
+    BN_hex2bn(&a_, a_hex.c_str());
+    d_ = BN_new();
+    BN_hex2bn(&d_, d_hex.c_str());
+    finite_field_ = BN_new();
+    BN_hex2bn(&finite_field_, finite_field_hex.c_str());
+    origin_ = Point();
+    g_ = Point(gx_hex, gy_hex);
+    n_ = BN_new();
+    BN_hex2bn(&n_, n_hex.c_str());
+}
+
+void EdwardsCurve::deleteCurve(){
+    BN_clear_free(a_);
+    BN_clear_free(d_);
+    BN_clear_free(finite_field_);
+    origin_.deletePoint();
+    g_.deletePoint();
+    BN_clear_free(n_);
+}
+
+BIGNUM* EdwardsCurve::getN(){
+    return n_;
+}
+
+Point EdwardsCurve::getG(){
+    return g_;
+}
+
+std::string EdwardsCurve::getEquation(){
+    std::string result = "";
+    result.append(BN_bn2hex(a_));
+    result.append("x^2 + y^2 = 1 + ");
+    result.append(BN_bn2hex(d_));
+    result.append("x^2y^2");
+    return result;
+}
+
+void EdwardsCurve::printCurveDetails(){
+    char* finite_field_str = BN_bn2hex(finite_field_);
+    printf("This is an edwards curve with the equation %s in the finite field %s\n", getEquation().c_str(),finite_field_str);
+}
+
+bool EdwardsCurve::validatePointOnCurve(Point p){
+    bool result = false;
+    if (p == origin_) {
+        result = true;
+    } else {
+        BN_CTX* calc_ctx = BN_CTX_new();
+        BN_CTX_start(calc_ctx);
+        BIGNUM *x = BN_CTX_get(calc_ctx);
+        BIGNUM *y = BN_CTX_get(calc_ctx);
+        BN_copy(x, p.getXAsBN());
+        BN_copy(y, p.getYAsBN());
+        bool x_in_field = BN_cmp(x, finite_field_) == -1;
+        bool y_in_field = BN_cmp(y, finite_field_) == -1;
+        if (x_in_field && y_in_field) {
+            //ax^2 + y^2 = 1 + dx^2y^2
+            BIGNUM* x_squared = BN_CTX_get(calc_ctx);
+            BIGNUM* y_squared = BN_CTX_get(calc_ctx);            
+            BIGNUM* left_side = BN_CTX_get(calc_ctx);
+            BIGNUM* right_side = BN_CTX_get(calc_ctx);
+
+            BN_mul(x_squared, x, x, calc_ctx);
+            BN_mul(left_side, x_squared, a_, calc_ctx);
+            BN_mul(y_squared, y, y, calc_ctx);
+            BN_add(left_side, left_side, y_squared);
+            calculatePositiveMod(left_side, finite_field_, calc_ctx);
+
+            BN_mul(right_side, x_squared, y_squared, calc_ctx);
+            BN_mul(right_side, d_, right_side, calc_ctx);
+            BN_add_word(right_side, 1);
+            calculatePositiveMod(right_side, finite_field_, calc_ctx);
+
+            int cmp_result = BN_cmp(left_side, right_side);
+            if(cmp_result == 0){
+                result = true;
+            }
+        }
+        BN_CTX_end(calc_ctx);
+        BN_CTX_free(calc_ctx);
+    }
+    return result;
+}
+
+std::string EdwardsCurve::getFiniteFieldAsHex(){
+    std::string result = std::string(BN_bn2hex(finite_field_));
+    return result;
+}
+
+std::string EdwardsCurve::getAAsHex(){
+    std::string result = std::string(BN_bn2hex(a_));
+    return result;
+}
+        
+std::string EdwardsCurve::getBAsHex(){
+    std::string result = std::string(BN_bn2hex(d_));
+    return result;
+}
+
+Point EdwardsCurve::calculatePointMultiplicationByConstant(Point p, BIGNUM* k){
+    Point r;
+
+    if (p == origin_ || BN_is_zero(k) == 1) {
+        r = Point();
+    } else if (BN_is_one(k) == 1){
+        r = Point(p);
+    } else {
+        r = Point(p);
+        int bit_length = BN_num_bits(k);
+        // printf("bit length is %d\n", bit_length);
+        for (int i = 1; i < bit_length; i ++){
+            int bit_value = BN_is_bit_set(k, bit_length - i - 1);
+            // printf("bit is %d\n", bit_value);
+            r = calculatePointAddition(r, r);
+            if(bit_value == 1){
+                r = calculatePointAddition(r, p);
+            }
+        }
+    }
+    return r;
+}
+
+Point EdwardsCurve::calculatePointInverse(Point p){
+    Point point;
+    if (p == origin_) {
+        point = Point(origin_);
+    } else {
+        BN_CTX *calc_ctx = BN_CTX_new();
+        BIGNUM *y_r = BN_new();
+        BN_copy(y_r, p.getYAsBN());
+        if(BN_is_negative(y_r) == 1){
+            BN_set_negative(y_r, 0);
+        } else {
+            BN_set_negative(y_r, 1);
+        }
+        BN_mod(y_r, y_r, finite_field_, calc_ctx);
+        if(BN_is_negative(y_r) == 1){
+            BN_add(y_r, finite_field_, y_r);
+        }
+        calculatePositiveMod(y_r, finite_field_, calc_ctx);
+        point = Point(p.getXAsBN(), y_r);
+        BN_clear_free(y_r);
+        BN_CTX_free(calc_ctx);
+    }
+    return point;
+}
+
+Point EdwardsCurve::calculatePointAddition(Point p, Point q){
+    // printf("starting point addition\n");
+    Point point;
+    if (validatePointOnCurve(p) && validatePointOnCurve(q)) {
+        if(p == origin_){
+            point = Point(q);
+        } else if (q == origin_){
+            point = Point(p);
+        } else if (p == calculatePointInverse(q)) {
+            point = Point(origin_);
+        } else {
+            BN_CTX *calc_ctx = BN_CTX_new();
+            BN_CTX_start(calc_ctx);
+
+            BIGNUM *x_p = BN_CTX_get(calc_ctx);
+            BIGNUM *y_p = BN_CTX_get(calc_ctx);
+            BIGNUM *x_q = BN_CTX_get(calc_ctx);
+            BIGNUM *y_q = BN_CTX_get(calc_ctx);
+
+            BIGNUM *dydx = BN_CTX_get(calc_ctx);
+            BIGNUM *mod_inv = BN_CTX_get(calc_ctx);
+
+            BIGNUM *x_r = BN_CTX_get(calc_ctx);
+            BIGNUM *y_r = BN_CTX_get(calc_ctx);
+
+            BN_copy(x_p, p.getXAsBN());
+            BN_copy(y_p, p.getYAsBN());
+            BN_copy(x_q, q.getXAsBN());
+            BN_copy(y_q, q.getYAsBN());
+
+            if (p == q) {
+                // dydx = (3 * x_p**2 + a) * ModInv(2 * y_p, finite_field)
+                BN_copy(mod_inv, y_p);
+                BN_mul_word(mod_inv, 2);
+                BN_mod_inverse(mod_inv, mod_inv, finite_field_, calc_ctx);
+                 /// printf("mod_inv = %s\n", BN_bn2dec(mod_inv));
+                BN_mul(dydx, x_p, x_p, calc_ctx);
+                BN_mul_word(dydx, 3);
+                BN_add(dydx, dydx, a_);
+                BN_mul(dydx, dydx, mod_inv, calc_ctx);
+                
+            } else {
+                // dydx = (y_q - y_p) * ModInv(x_q - x_p, finite_field)
+                BN_sub(mod_inv, x_q, x_p);
+                BN_mod_inverse(mod_inv, mod_inv, finite_field_, calc_ctx);
+                BN_sub(dydx, y_q, y_p);
+                BN_mul(dydx, dydx, mod_inv, calc_ctx);
+            }
+            calculatePositiveMod(dydx, finite_field_, calc_ctx);
+            // printf("dydx = %s\n", BN_bn2dec(dydx));
+            // x_r = (dydx**2 - x_p - x_q) % self.finite_field
+            BN_mul(x_r, dydx, dydx, calc_ctx);
+            BN_sub(x_r, x_r, x_p);
+            BN_sub(x_r, x_r, x_q);
+            calculatePositiveMod(x_r, finite_field_, calc_ctx);
+
+            // y_r = (dydx * (x_p - x_r) - y_p) % finite_field
+            BN_copy(y_r, x_p);
+            // printf("y_r = %s\n", BN_bn2dec(y_r));
+            BN_sub(y_r, y_r, x_r);
+            // printf("y_r = %s\n", BN_bn2dec(y_r));
+            BN_mul(y_r, y_r, dydx, calc_ctx);
+            // printf("y_r = %s\n", BN_bn2dec(y_r));
+            BN_sub(y_r, y_r, y_p);
+            // printf("y_r = %s\n", BN_bn2dec(y_r));
+            calculatePositiveMod(y_r, finite_field_, calc_ctx);
+            
+            Point potential = Point(x_r, y_r);
+            // printf("x_r = %s\n", BN_bn2dec(x_r));
+            // printf("y_r = %s\n", BN_bn2dec(y_r));
+            if(validatePointOnCurve(potential)){
+                point = potential;
+            }
+
+            BN_CTX_end(calc_ctx);
+            BN_CTX_free(calc_ctx);
+        }
+    }
+    return point;
+}
+
+class edwards25519 : public EdwardsCurve{
+    public  : 
+        edwards25519() : EdwardsCurve(
+            "-1",   // a (x coefficient)
+            "52036cee2b6ffe738cc740797779e89800700a4d4141d8ab75eb4dca135978a3",   // d (y coefficient)
+            "1000000000000000000000000000000014DEF9DEA2F79CD65812631A5CF5D3ED",   // p (finite field)
+            "216936d3cd6e53fec0a4e231fdd6dc5c692cc7609525a7b2c9562d608f25d51a",   // g_x (The x coordinate of the generator point)
+            "6666666666666666666666666666666666666666666666666666666666666658",   // g_y (The y coordinate of the generator point)
+            "7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFED"   // n (The order of the curve)
+        ){};
+};
