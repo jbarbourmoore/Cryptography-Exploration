3d8859452a9275093f6686f66f8e01f9cc46020d
Jamie <190376747+jbarbourmoore@users.noreply.github.com>
2025-07-03 20:07:36 -0400

    Added test for sha3 pi

--- C++_Implementations/Tests/HashingTests/src/SHA3Methods_tests.cpp
+++ C++_Implementations/Tests/HashingTests/src/SHA3Methods_tests.cpp
@@ -44,3 +44,24 @@ TEST(SHA3Methods_Tests, sha3_rho) {
     
     EXPECT_EQ(after_rho, hex_result);
 }
+
+TEST(SHA3Methods_Tests, sha3_pi) {
+    // test values taken from https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/SHA3-224_Msg5.pdf
+    
+    std::string after_rho = "D3 00 00 00 00 00 00 00 A4 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 08 00 00 00 00 00 00 00 30 0D 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 20 0D 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 40 00 00 00 60 1A 00 00 00 00 00 00 00 00 00 00 00 00 00 48 03 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 00 00 D3 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 40 1A 00 00 40 00 00 00 00 00 00 00 00 10 00 00 00 00 00 00 A6 01 00 00 00 00 00 00 00 00 00 00 00 00 00 48 03 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 80 00 00 80 69 00 00 00 00 00 ";
+    
+    std::string after_pi = "D3 00 00 00 00 00 00 00 00 00 00 00 00 20 0D 00 00 00 00 00 00 00 00 00 00 00 10 00 00 00 00 00 00 80 69 00 00 00 00 00 00 00 00 08 00 00 00 00 00 00 60 1A 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 40 1A 00 00 00 00 00 00 00 00 00 A4 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 00 00 A6 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 30 0D 00 00 00 00 00 00 00 00 00 00 00 00 48 03 00 00 00 00 00 00 40 00 00 00 00 00 00 00 00 00 00 00 00 80 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 40 00 00 00 00 00 00 D3 00 00 00 00 00 00 00 00 00 00 48 03 00 00 00 00 00 00 ";
+
+    after_rho.erase(std::remove(after_rho.begin(), after_rho.end(), ' '), after_rho.end());
+    after_pi.erase(std::remove(after_pi.begin(), after_pi.end(), ' '), after_pi.end());
+
+    SHA3_State state = SHA3_State(after_rho);
+    state.printHex();
+    state.printBits();
+    state.pi();
+    state.printHex();
+
+    std::string hex_result = state.getValueAsHex();
+    
+    EXPECT_EQ(after_pi, hex_result);
+}
