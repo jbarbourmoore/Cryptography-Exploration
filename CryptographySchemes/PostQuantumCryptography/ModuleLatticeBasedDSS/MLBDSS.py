4d6bee9c7e64f909549df436ef04368482e5702a
Jamie <jbarbourmoore@gmail.com>
2025-08-31 15:14:17 -0400

    Added IntegerToBytes
    Algorithm 11 of NIST FIPS 204

--- CryptographySchemes/PostQuantumCryptography/ModuleLatticeBasedDSS/MLBDSS.py
+++ CryptographySchemes/PostQuantumCryptography/ModuleLatticeBasedDSS/MLBDSS.py
@@ -3,15 +3,17 @@
     This funtion creates a bit array representation of x mod 2 ** alpha
     According to Algorithm 9 of NIST FIPS 204
 
-        Parameters : 
-            x : int
-                The value that is to be represented as a bit array
-            alpha : int 
-                The length of the bit array / modulus value for the representation
-                
-        Returns : 
-            y : [int]
-                The bit array representing the value for x
+    note: little endian
+
+    Parameters : 
+        x : int
+            The value that is to be represented as a bit array
+        alpha : int 
+            The length of the bit array / modulus value for the representation
+            
+    Returns : 
+        y : [int]
+            The bit array representing the value for x
 '''
 def IntegerToBits(x, alpha):
     x_prime = x
@@ -25,18 +27,44 @@ def IntegerToBits(x, alpha):
     This funtion transforms a bit array into th integer representative
     According to Algorithm 10 of NIST FIPS 204
 
-        Parameters : 
-            y : [int]
-                The bit array representing the value for x
-            alpha : int 
-                The length of the bit array / modulus value for the representation
-                
-        Returns : 
-            x : int
-                The integer value that was represented as a bit array
+    note: little endian
+
+    Parameters : 
+        y : [int]
+            The bit array representing the value for x
+        alpha : int 
+            The length of the bit array / modulus value for the representation
+            
+    Returns : 
+        x : int
+            The integer value that was represented as a bit array
 '''
 def BitsToInteger(y, alpha):
     x = 0
     for i in range (1, alpha):
         x = 2 * x + y[alpha - 1]
     return x
+
+'''
+    This funtion creates a bit array representation of x mod 256 ** alpha
+    According to Algorithm 11 of NIST FIPS 204
+
+    note: little endian
+
+    Parameters : 
+        x : int
+            The value that is to be represented as a byte array
+        alpha : int 
+            The length of the byte array / modulus value for the representation
+            
+    Returns : 
+        y : [int]
+            The byte array representing the value for x
+'''
+def IntegerToBytes(x, alpha):
+    x_prime = x
+    y = [0] * alpha
+    for i in range (0, alpha):
+        y[i] = x_prime % 256
+        x_prime = x_prime / 256
+    return y
