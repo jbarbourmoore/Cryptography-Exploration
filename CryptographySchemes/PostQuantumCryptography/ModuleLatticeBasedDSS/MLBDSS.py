70be52898da124c5cdedaef15154e41edd5c7aee
Jamie <jbarbourmoore@gmail.com>
2025-08-31 15:08:37 -0400

    Added BitsToInteger
    Algorithm 10 of NIST FIPS 204

--- CryptographySchemes/PostQuantumCryptography/ModuleLatticeBasedDSS/MLBDSS.py
+++ CryptographySchemes/PostQuantumCryptography/ModuleLatticeBasedDSS/MLBDSS.py
@@ -20,3 +20,23 @@ def IntegerToBits(x, alpha):
         y[i] = x_prime % 2
         x_prime = x_prime / 2
     return y
+
+'''
+    This funtion transforms a bit array into th integer representative
+    According to Algorithm 10 of NIST FIPS 204
+
+        Parameters : 
+            y : [int]
+                The bit array representing the value for x
+            alpha : int 
+                The length of the bit array / modulus value for the representation
+                
+        Returns : 
+            x : int
+                The integer value that was represented as a bit array
+'''
+def BitsToInteger(y, alpha):
+    x = 0
+    for i in range (1, alpha):
+        x = 2 * x + y[alpha - 1]
+    return x
